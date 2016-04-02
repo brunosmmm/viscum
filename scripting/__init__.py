e0d53a3450faf715ce2693ab2f307fa48fe35d8c
brunosmmm <brunosmmm@gmail.com>
2016-04-02 14:28:44 -0400

    distinguish between multi and single isntance plugins

--- scripting/__init__.py
+++ scripting/__init__.py
@@ -4,15 +4,19 @@ import ast
 import symtable
 import codegen
 import time
+from periodicpy.plugmgr.plugin import ModuleCapabilities
 
 class InvalidModuleError(Exception):
+    pass
+
+class DeferScriptLoading(Exception):
     pass
 
 class ModuleManagerScript(object):
     """Script parser and container class
     """
 
-    def __init__(self, source_file, module_manager):
+    def __init__(self, source_file, module_manager, initialize=False):
 
         #module_manager reference
         self._modman = module_manager
@@ -31,6 +35,10 @@ class ModuleManagerScript(object):
 
         #flags
         self._executed_once = False
+
+        #auto-initialize
+        if initialize:
+            self.initialize_script()
 
     @staticmethod
     def sanitize_code(code, logging_function=None):
@@ -56,14 +64,27 @@ class ModuleManagerScript(object):
             for symbol in symtab.get_symbols():
                 if symbol.is_referenced() and symbol.is_assigned() == False and symbol.is_parameter() == False:
                     #tried to reference a symbol but was never assigned? Investigate
-                    symbol_name = symbol.get_name()
+                    name_parts = symbol.get_name().split('__')
+                    module_type = name_parts[0]
+                    instance_name = ''.join(name_parts[1:])
+                    if instance_name != '':
+                        symbol_name = '{}-{}'.format(module_type, instance_name)
+                    else:
+                        symbol_name = module_type
                     if symbol_name in self._modman.list_loaded_modules():
                         #this is a module name
                         required_modules.add(symbol_name)
-                    elif symbol_name in self._modman.list_discovered_modules():
+                    elif module_type in self._modman.list_discovered_modules():
                         #this is a module type but it is not loaded at the moment
                         #TODO: defer the loading of this script somehow, raise some exception perhaps
-                        pass
+                        #if plugin has multi-instance capability, the name without a suffix
+                        #accesses the class
+                        if ModuleCapabilities.MultiInstanceAllowed in self._modman.get_module_capabilities(module_type) and instance_name != '':
+                            raise DeferScriptLoading({'type': module_type, 'inst': instance_name})
+                        elif ModuleCapabilities.MultiInstanceAllowed not in self._modman.get_module_capabilities(module_type):
+                            raise DeferScriptLoading({'type': module_type, 'inst': module_type})
+                        else:
+                            required_modules.add(module_type)
                     elif symbol_name in global_scope:
                         #being provided by the global scope, allowed
                         continue
@@ -96,7 +117,9 @@ class ModuleManagerScript(object):
     def _script_print_statement(self, *args):
         """Logs a print statement executed by script
         """
-        message = ''.join(args)
+        #try to transform everything in strings
+        msg_list = [str(arg) for arg in args]
+        message = ''.join(msg_list)
 
         self._modman.log_message('log_info', 'scripting: {}'.format(message))
 
@@ -135,7 +158,7 @@ class ModuleManagerScript(object):
 
     def _execute_code(self, instrumented_code):
         """Executes the script body. Should only be done once, normally
-           Typically this will attack callbacks and do initialization
+           Typically this will attach callbacks and do initialization
         """
         compiled_script = compile(instrumented_code, '<string>', 'exec')
         exec compiled_script in self.script_scope
@@ -187,25 +210,50 @@ class CodeSanitizer(ast.NodeTransformer):
 class ModuleProxy(object):
     """Module proxy class to invoke methods and read properties transparently
     """
-    def __init__(self, module_name, module_manager):
+    def __init__(self, module_name, module_manager, instance_name=None):
         self._modman = module_manager
         self._modname = module_name
+        self._instname = instance_name
+
+    def get_available_instances(self):
+        return self._modman.get_instance_list_by_type(self._modname)
+
+    def get_instance(self, instance_name):
+        _instance_name = '{}-{}'.format(self._modname, instance_name)
+        if _instance_name in self.get_available_instances():
+            return ModuleProxy(self._modname, self._modman, instance_name)
+
+        raise DeferScriptLoading({'type': self._modname, 'inst': instance_name})
 
     def __getattr__(self, name):
         #search for the requested attribute, look into properties and methods
 
-        property_list = self._modman.get_module_property_list(self._modname)
-        method_list = self._modman.get_module_method_list(self._modname)
+        #try to get instance name
+        if self._instname == None:
+            name_parts = self._modname.split('__')
+            module_type = name_parts[0]
+            instance_suffix = ''.join(name_parts[1:])
+            if instance_suffix != '':
+                instance_name = '{}-{}'.format(module_type, instance_suffix)
+            else:
+                instance_name = module_type
+        else:
+            module_type = self._modname
+            instance_name = self._instname
+            instance_name = '{}-{}'.format(module_type, instance_name)
+
+        property_list = self._modman.get_module_property_list(module_type)
+        method_list = self._modman.get_module_method_list(module_type)
+
         if name in property_list:
             #property found, get property value and return
-            return self._modman.get_module_property(self._modname, name)
+            return self._modman.get_module_property(instance_name, name)
         elif name in method_list:
             #method found
             def method_proxy(*args, **kwargs):
-                return self._modman.call_module_method(self._modname, name, *args, **kwargs)
+                return self._modman.call_module_method(instance_name, name, *args, **kwargs)
 
             return method_proxy
-
         else:
             #not found!
             raise AttributeError
